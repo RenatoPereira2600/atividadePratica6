302a3c3dc3ccec07b6902ba61d43c17871463157
RenatoPereira2600 <48362272+RenatoPereira2600@users.noreply.github.com>
2021-09-26 22:20:31 -0300

    Subindo atividade pratica 6

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,57 @@
+import { Component } from '@angular/core';
+import { ToastController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  weight: number;
+  height: number;
+
+  constructor(private toastController: ToastController) {}
+
+  isFormValid() {
+    return (this.height && this.weight && this.height > 0 && this.weight > 0);
+  }
+  category(imc:number){
+    if(imc<18.5){
+      return 'MAGREZA';
+    }else if(imc>=18.5 && imc<=24.9){
+      return 'NORMAL';
+    }else if(imc>=25.0 && imc<=29.9){
+      return 'SOBREPESO';
+    }else if(imc>=30.0 && imc<=39.9){
+      return 'OBESIDADE';
+    }else{
+      return 'OBESIDADE GRAVE';
+    }
+  }
+  
+  onCalculate() {
+    const imc = this.weight / (this.height * this.height);
+    this.showMessage(`IMC = ${imc.toFixed(2)} - ${this.category(imc)}`);
+  }
+
+  async showMessage(msg: string) {
+    const previousToast = await this.toastController.getTop();
+    if (previousToast) {
+      await this.toastController.dismiss();
+    }
+
+    const toast = await this.toastController.create(
+      {
+        message: msg,
+        color: 'light',
+        buttons: [
+          {
+            icon: 'close'
+          }
+        ]
+      }
+    );
+    toast.present();
+  }
+}
